c90189f4d06666ff365858ec8b702d4e46d2cd97
Chris Alejandro Palabay <acpalabay@gmail.com>
2021-04-05 13:34:34 +0800

    Exploring Graph with generics

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -1,91 +1,9 @@
 package Graph;
 
 
-import java.util.LinkedList;
-
-public class Graph{
-
-    int vertices;
-    LinkedList<Node> list[];
-
-    public Graph(int vertices){
-        this.vertices = vertices;
-        list = new LinkedList[vertices];
-        for(int i=0;i < vertices;i++){
-            list[i] = new LinkedList<Node>();
-        }
-    }
-
-    public void addEdge(Node s, Node d){
-        list[getIndex(list,s)].add(d);
-        list[getIndex(list,d)].add(s);
-    }
-
-    public void displayGraph(){
-        for(int i=0;i < list.length;i++){
-            System.out.println(list[i]+" is connected to ");
-            if(list[i].size() > 0){
-                for(int j=0;j < list[i].size();j++){
-                    System.out.println(list[i].get(j));
-                }
-            }
-        }
-    }
 
 
-    public int getIndex(LinkedList<Node> arrayNode[],Node node){
-        int index = 0;
-        for(int i=0;i<arrayNode.length;i++){
-            if(arrayNode[i].get(i) == node.data ){
-                index = i;
-            }
-        }
-        return index;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-/**
 public class Graph {
 
-    int vertices;
-    LinkedList<Integer> adjList[];
-
-    public Graph(int vertices){
-        this.vertices = vertices;
-        adjList = new LinkedList[vertices];
-        for(int i=0;i< vertices;i++){
-            adjList[i] = new LinkedList<Integer>();
-        }
-    }
-
-    public void addEdge(int source, int destination){
-        adjList[source].add(destination);
-        adjList[destination].add(source);
-    }
-
-    public void displayGraph(){
-        for(int i=0;i < vertices;i++){
-            if(adjList[i].size() > 0){
-                System.out.println("Vertex " + i +" is connected to - ");
-                for(int j=0;j< adjList[i].size();j++){
-                    System.out.print(adjList[i].get(j)+",");
-                }
-                System.out.println();
-            }
-        }
-    }
-
 
 }
- ***/
--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -1,14 +1,29 @@
 package Graph;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Graph graph = new Graph(2);
+        ArrayList<ArrayList<Node>> list = new ArrayList<ArrayList<Node>>();
 
-        
+        ArrayList<Node> people = new ArrayList<Node>();
+
+
+
+        people.add(new Node(new FamilyMember("Ac",21)));
+        people.add(new Node(new FamilyMember("Aundrea",21)));
+        people.add(new Node(new FamilyMember("Nicole",21)));
+
+        list.add(people);
+
+        for(int i=0;i<list.get(0).size();i++){
+            System.out.println(list.get(0).get(i).data);
+        }
+
+
+
 
 
 
