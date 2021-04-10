f5c7ff656fe16cc8c6afb53a6e262e44e970a63e
Chris Alejandro Palabay <acpalabay@gmail.com>
2021-04-10 21:35:37 +0800

    Graph Data Structure test

--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -10,6 +10,12 @@ public class Main {
 
         Graph graph = new Graph();
         graph.addEdge("AC","Aundrea",true);
+        graph.addEdge("Nicole","Aundrea",true);
+        graph.addEdge("Draku","Nic",true);
+        graph.addEdge("Chris","lo",true);
+        graph.addEdge("Alejandro","e",true);
         graph.printGraph();
+
+        System.out.println();
     }
 }
