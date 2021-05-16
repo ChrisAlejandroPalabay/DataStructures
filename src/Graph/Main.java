3ebd0702c33c9d18b5eb2a7f1a78640153c99120
Chris Alejandro Palabay <acpalabay@gmail.com>
2021-05-16 12:06:33 +0800

    Validation

--- src/Graph/Main.java
+++ src/Graph/Main.java
@@ -11,10 +11,13 @@ public class Main {
         graph.addFamilyMember(new FamilyMember("Ac"));
         graph.addFamilyMember(new FamilyMember("Draku"));
         graph.addFamilyMember(new FamilyMember("Charcoal"));
+        graph.addFamilyMember(new FamilyMember("Nica"));
 
 
         graph.addSibling(new FamilyMember("Ac"),new FamilyMember("Draku"));
         graph.addFather(new FamilyMember("Draku"),new FamilyMember("Charcoal"));
+        graph.addMother(new FamilyMember("Ac"),new FamilyMember("Nica"));
+        graph.addChild(new FamilyMember("Charcoal"),new FamilyMember("Ac"));
 
 
         System.out.println(graph.print());
